39cef61cf2d94af51732f17e64957fcd1ebf0244
BhavyanshM <bhavyanshmishra@gmail.com>
2019-10-18 14:57:00 -0500

    Add some useful functions

--- project.py
+++ project.py
@@ -9,35 +9,57 @@ f, cx, cy = 512, 256, 256
 K = np.array([[f, 0, cx],[0, f, cy],[0, 0, 1]])
 
 N = 8
-P_M = np.array([	[1, -1, 1, -1, 1, -1, 1, -1], 
-		[1, 1, -1, -1, 1, 1, -1, -1], 
-		[1, 1, 1, 1, -1, -1, -1, -1], 
-		[1, 1, 1, 1, 1, 1, 1, 1	]		])
+P_M = np.array([[1, -1, 1, -1, 1, -1, 1, -1], 
+				[1, 1, -1, -1, 1, 1, -1, -1], 
+				[1, 1, 1, 1, -1, -1, -1, -1], 
+				[1, 1, 1, 1, 1, 1, 1, 1	]	])
 
-ax, ay, az = 0, 20*math.pi/180, -30*math.pi/180
-tx, ty, tz = 0, 0, 6
-Rx = np.array([	[1,			0,			0		],
-				[0,			math.cos(ax), 	-math.sin(ax)],
-				[0, 		math.sin(ax), 	math.cos(ax)	]	])
+rdx, rdy, rdz = 0, 0, 0
+tx, ty, tz = 0, 6, 6
 
-Ry = np.array([	[math.cos(ay), 	0, 		math.sin(ay)],
-				[0, 			1, 		0],
-				[-math.sin(ay), 0, 	math.cos(ay)]	])
-
-Rz = np.array([	[math.cos(az), 	-math.sin(az), 	0],
-				[math.sin(az), 	math.cos(az), 	0],
-				[0, 			0, 				1]			])
+def degToRad(dx,dy,dz)
+	return ax, ay, az = (rdx)*math.pi/180, (rdy)*math.pi/180, (rdz)*math.pi/180
 
 
-Rtrue = Rz @ Ry @ Rx
-T = np.array([tx, ty, tz])
-M_ext = np.column_stack((Rtrue, T))
+def rotX(ax):
+	return np.array([	[1,			0,			0		],
+					[0,			math.cos(ax), 	-math.sin(ax)],
+					[0, 		math.sin(ax), 	math.cos(ax)	]	])
 
-p = K @ M_ext @ P_M
+def rotY(ay):
+	return np.array([	[math.cos(ay), 	0, 		math.sin(ay)],
+					[0, 			1, 		0],
+					[-math.sin(ay), 0, 	math.cos(ay)]	])
 
-p[0,:] = np.divide(p[0,:], p[2,:])
-p[1,:] = np.divide(p[1,:], p[2,:])
-p[2,:] = np.divide(p[2,:], p[2,:])
+def rotZ(az):
+	return np.array([	[math.cos(az), 	-math.sin(az), 	0],
+					[math.sin(az), 	math.cos(az), 	0],
+					[0, 			0, 				1]			])
+
+def rotXYZ(ax, ay, az):
+	return rotZ(az) @ rotY(ay) @ rotX(ax)
+
+def transXYZ(bx, by, bz):
+	return np.array([bx, by, bz])
+
+def project(K, M, Points):
+	return K @ M @ Points
+
+def unscaleH2D(points):
+	points[0,:] = np.divide(points[0,:], points[2,:])
+	points[1,:] = np.divide(points[1,:], points[2,:])
+	points[2,:] = np.divide(points[2,:], points[2,:])
+	return points
+
+
+
+ax, ay, az = degToRad(rdx, rdy, rdz)
+R_tot = rotXYZ(ax, ay, az)
+T = transXYZ(tx, ty, tz)
+M_ext = np.column_stack((R_tot, T))
+p = project(K, M_ext, P_M)
+p = unscaleH2D(p)
+
 
 A = np.zeros((N*2, 12))
 
