a05006c1a9d098f054cabef3446a02c21bcb5254
BhavyanshM <bhavyanshmishra@gmail.com>
2019-11-24 03:27:01 -0600

    Add all the files

--- optical_diff.py
+++ optical_diff.py
@@ -2,33 +2,47 @@ import cv2
 import numpy as np
 import time
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
+cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+cap.set(cv2.CAP_PROP_EXPOSURE, -4)
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
 # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
 	# img = cv2.imread("Chess.JPG")
 
-ret, temp = cap.read()
 # ret, prev = cv2.threshold(temp, 127, 255, cv2.THRESH_BINARY)
-prev = temp
 prevTime = time.time()
 num_frames = 0
 total_time = 0
 
+print("Sleeping")
+time.sleep(5)
+ret, first = cap.read()
+print("Captured!")
+
 while True:
 	num_frames += 1
-	ret, temp = cap.read()
+	ret, img = cap.read()
 	# ret, img = cv2.threshold(temp, 127, 255, cv2.THRESH_BINARY)
-	img = temp
 	# img = img[200:650,200:650,:]
 
 
-	diff = cv2.subtract(img, prev)
+	diff = cv2.absdiff(img, first)
+	mask = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+	thresh = 10
+	imask = mask>thresh
+	canvas = np.zeros_like(img, np.uint8)
+	canvas[imask] = img[imask]
+
+
 
 	cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
 	cv2.resizeWindow('Result', 1800,1350)
-	cv2.imshow("Result", diff)
-	prev = img
+	cv2.imshow("Result", canvas)
 
+	cv2.namedWindow('First', cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('First', 1800,1350)
+	cv2.imshow("First", first)
 
 
 	curTime = time.time()
--- stereo.py
+++ stereo.py
@@ -1,17 +1,84 @@
 import cv2
 import numpy as np
 import time
+import math
+import matplotlib.pyplot as plt
+from scipy.optimize import least_squares
 
-cap1 = cv2.VideoCapture(1)
-cap2 = cv2.VideoCapture(2)
+cap1 = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(1)
 cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 512)
 cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 384)
 cap2.set(cv2.CAP_PROP_FRAME_WIDTH, 512)
 cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, 384)
 
+n_cams, n_points, n_obs = 2, 200, 400
 
-	# img = cv2.imread("Chess.JPG")
+init_params = np.random.random(n_cams*9 + n_points*3)
+points_2d = np.ones((n_obs, 2), dtype=int)
+cam_indices = np.zeros(n_obs, dtype=int)
+point_indices = np.zeros(n_obs, dtype=int)
 
+def cameraMatrix(f, cx, cy):
+	return np.array([[f, 0, cx],[0, f, cy],[0, 0, 1]])
+
+def cartesian2d(points):
+	points[:,0] = np.divide(points[:,0], points[:,2])
+	points[:,1] = np.divide(points[:,1], points[:,2])
+	points[:,2] = np.divide(points[:,2], points[:,2])
+	return points
+
+def degToRad(dx,dy,dz):
+	return (rdx)*math.pi/180, (rdy)*math.pi/180, (rdz)*math.pi/180
+
+
+def rotX(ax):
+	return np.array([	[1,			0,			0		],
+					[0,			math.cos(ax), 	-math.sin(ax)],
+					[0, 		math.sin(ax), 	math.cos(ax)	]	])
+
+def rotY(ay):
+	return np.array([	[math.cos(ay), 	0, 		math.sin(ay)],
+					[0, 			1, 		0],
+					[-math.sin(ay), 0, 	math.cos(ay)]	])
+
+def rotZ(az):
+	return np.array([	[math.cos(az), 	-math.sin(az), 	0],
+					[math.sin(az), 	math.cos(az), 	0],
+					[0, 			0, 				1]			])
+
+def rotXYZ(ax, ay, az):
+	return rotZ(az) @ rotY(ay) @ rotX(ax)
+
+def rotate(points, rots):
+	rot_points = np.zeros_like(points)
+	for i in range(len(points)):
+		rot_points[i] = points[i] @ rotXYZ(rots[i,0], rots[i,1], rots[i,2])
+	return rot_points
+
+
+def project2d(points, camera_params):
+	"""Convert 3-D points to 2-D by projecting onto images."""
+	points_proj = rotate(points, camera_params[:, :3])
+	points_proj += camera_params[:, 3:6]
+	points_proj = points_proj[:, :2] / points_proj[:, 2, np.newaxis]
+	f = camera_params[:, 6]
+	k1 = camera_params[:, 7]
+	k2 = camera_params[:, 8]
+	n = np.sum(points_proj**2, axis=1)
+	r = 1 + k1 * n + k2 * n**2
+	points_proj *= (r * f)[:, np.newaxis]
+	return points_proj
+
+def residuals(params, n_cams, n_points, cam_indices, point_indices, points_2d):
+	"""Generate residuals between observed and reprojected points"""
+	camera_params = params[:n_cams * 9].reshape((n_cams, 9))
+	points_3d = params[n_cams * 9:].reshape((n_points, 3))
+	points_proj = project2d(points_3d[point_indices], camera_params[cam_indices])
+	return (points_proj - points_2d).ravel()
+
+
+correspondences = []
 px1, px2, py1, py2 = 0, 0, 0, 0
 
 while True:
@@ -19,14 +86,6 @@ while True:
 	assert ret1
 	ret2, img2 = cap2.read()
 	assert ret2
-
-
-	# res = cv2.add(res, resG)
-	# res = cv2.add(res, resB)
-	cv2.namedWindow("Image1", cv2.WINDOW_NORMAL)
-	cv2.namedWindow("Image2", cv2.WINDOW_NORMAL)
-	cv2.resizeWindow("Image1", 800, 600)
-	cv2.resizeWindow("Image2", 800, 600)
 
 	lower_red = np.array([47, 78, 93])
 	upper_red = np.array([72, 162, 245])
@@ -43,7 +102,6 @@ while True:
 	cY1 = int(M["m01"] / M["m00"]) if M["m00"] != 0 else py1
 	cv2.line(mask1, (cX1 + 20, cY1), (cX1 - 20, cY1), 255, 1)
 	cv2.line(mask1, (cX1, cY1 + 20), (cX1, cY1 - 20), 255, 1)
-	cv2.imshow('Image1', mask1)
 
 	mask2 = cv2.inRange(hsv2, lower_red, upper_red)
 	M = cv2.moments(mask2)
@@ -51,16 +109,58 @@ while True:
 	cY2 = int(M["m01"] / M["m00"]) if M["m00"] != 0 else py2
 	cv2.line(mask2, (cX2, cY2 + 20), (cX2, cY2 - 20), 255, 1)
 	cv2.line(mask2, (cX2 + 20, cY2), (cX2 - 20, cY2), 255, 1)
-	cv2.imshow('Image2', mask2)
 
-	px1, py1, px2, py2 = cX1, cY1, cX2, cY2
-	
+	corresp = px1, py1, px2, py2 = cX1, cY1, cX2, cY2
+	canvas1 = np.zeros_like(img1, np.uint8)
+	canvas2 = np.zeros_like(img2, np.uint8)
 
-	# cv2.imshow("Image1", img1)
-	# cv2.imshow("Image2", img2)
+	if np.sum(mask1) > 25000 and np.sum(mask2) > 25000 and len(correspondences) < n_points:
+		pn = len(correspondences)
+		points_2d[pn*2, :] = np.array([px1, py1])
+		points_2d[pn*2 + 1, :] = np.array([px2, py2])
+		point_indices[pn*2], point_indices[pn*2 + 1] = pn, pn
+		cam_indices[pn*2], cam_indices[pn*2 + 1] = 0, 1
+		correspondences.append(corresp)
 
+
+		for c in correspondences:
+			x1, y1, x2, y2 = c
+			c1 = (x1/(x1+y1)*255, y1/(x1+y1)*255, 255)
+			c2 = (x2/(x2+y2)*255, y2/(x2+y2)*255, 255)
+			cv2.circle(canvas1, (x1,y1), 5, c1, -1)
+			cv2.circle(canvas2, (x2,y2), 5, c1, -1)		
+		
+
+		cv2.namedWindow("Image1", cv2.WINDOW_NORMAL)
+		cv2.namedWindow("Image2", cv2.WINDOW_NORMAL)
+		cv2.resizeWindow("Image1", 1600, 900)
+		cv2.resizeWindow("Image2", 1600, 900)
+		cv2.imshow('Image1', canvas1)
+		cv2.imshow('Image2', canvas2)
+
+		print(pn)
+
+	if(len(correspondences) == n_points):
+		break
 	code = cv2.waitKeyEx(1)
 	if code == ord('q'):
 		break
 
+# res = residuals(init_params, n_cams, n_points, cam_indices, point_indices, points_2d)
+
 cv2.destroyAllWindows()
+print("Optimization started.")
+
+t0 = time.time()
+res = least_squares(residuals, init_params, verbose=2, ftol=1e-2, method='trf',args=(n_cams, n_points, cam_indices, point_indices, points_2d))
+t1 = time.time()
+
+print(res.x)
+print("Optimization took {0:.0f} seconds".format(t1 - t0))
+
+plt.rcParams.update({'font.size': 14})
+fig = plt.figure(figsize=(35,15))
+plt.plot(res.fun)
+plt.show()
+
+
