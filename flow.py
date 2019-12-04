2460049d168ed79f12a868a5db3effc78caaf628
BhavyanshM <bhavyanshmishra@gmail.com>
2019-12-04 13:08:02 -0600

    Add the double flow and optical_diff

--- flow.py
+++ flow.py
@@ -2,33 +2,45 @@ import cv2
 import numpy as np
 import time
 
-cap = cv2.VideoCapture(0)
+cap1 = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(1)
+
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
 # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
 	# img = cv2.imread("Chess.JPG")
 
-ret, temp = cap.read()
-# ret, prev = cv2.threshold(temp, 127, 255, cv2.THRESH_BINARY)
-prev = temp
+ret1, temp1 = cap1.read()
+prev1 = temp1
+
+ret2, temp2 = cap2.read()
+prev2 = temp2
+
+
 prevTime = time.time()
 num_frames = 0
 total_time = 0
 
 while True:
 	num_frames += 1
-	ret, temp = cap.read()
-	# ret, img = cv2.threshold(temp, 127, 255, cv2.THRESH_BINARY)
-	img = temp
-	# img = img[200:650,200:650,:]
+	ret1, temp1 = cap1.read()
+	img1 = temp1
+
+	ret2, temp2 = cap2.read()
+	img2 = temp2
 
 
-	diff = cv2.subtract(img, prev)
+	diff1 = cv2.subtract(img1, prev1)
+	diff2 = cv2.subtract(img2, prev2)
 
-	cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
-	cv2.resizeWindow('Result', 1800,1350)
-	cv2.imshow("Result", diff)
-	prev = img
+	cv2.namedWindow('Result1', cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('Result1', 800, 600)
+	cv2.imshow("Result1", diff1)
+	prev1 = img1
 
+	cv2.namedWindow('Result2', cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('Result2', 800, 600)
+	cv2.imshow("Result2", diff2)
+	prev2 = img2
 
 
 	curTime = time.time()
--- optical_diff.py
+++ optical_diff.py
@@ -2,13 +2,17 @@ import cv2
 import numpy as np
 import time
 
-cap = cv2.VideoCapture(0)
+cap1 = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(1)
 
+cap1.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+cap1.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+cap1.set(cv2.CAP_PROP_EXPOSURE, -4)
 
-cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
-cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
-cap.set(cv2.CAP_PROP_EXPOSURE, -4)
-time.sleep(2)
+cap2.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+cap2.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+cap2.set(cv2.CAP_PROP_EXPOSURE, -4)
+
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
 # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
 	# img = cv2.imread("Chess.JPG")
@@ -18,30 +22,55 @@ prevTime = time.time()
 num_frames = 0
 total_time = 0
 
-print("Sleeping")
 # time.sleep(5)
-ret, first = cap.read()
+ret1, first1 = cap1.read()
+ret2, first2 = cap2.read()
+set1, set2 = 0, 0
 print("Captured!")
 
 while True:
 	num_frames += 1
-	ret, img = cap.read()
+	ret1, img1 = cap1.read()
+	ret2, img2 = cap2.read()
 	# ret, img = cv2.threshold(temp, 127, 255, cv2.THRESH_BINARY)
 	# img = img[200:650,200:650,:]
 
 
-	diff = cv2.absdiff(img, first)
-	mask = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-	thresh = 15
-	imask = mask>thresh
-	canvas = np.zeros_like(img, np.uint8)
-	canvas[imask] = img[imask]
+	diff1 = cv2.absdiff(img1, first1)
+	mask1 = cv2.cvtColor(diff1, cv2.COLOR_BGR2GRAY)
+	thresh1 = 15
+	imask1 = mask1>thresh1
+	canvas1 = np.zeros_like(img1, np.uint8)
+	canvas1[imask1] = img1[imask1]
+
+	diff2 = cv2.absdiff(img2, first2)
+	mask2 = cv2.cvtColor(diff2, cv2.COLOR_BGR2GRAY)
+	thresh2 = 15
+	imask2 = mask2>thresh2
+	canvas2 = np.zeros_like(img2, np.uint8)
+	canvas2[imask2] = img2[imask2]
+
+	tot1, tot2 = np.sum(imask1), np.sum(imask2)
+
+	# print("Img1:", tot1, "\tImg2:", tot2)
+
+	# if tot1 > 1000 and set1 == 0:
+	# 	first1 = img1
+	# 	set1 = 1
+	# 	print("Changed Image 1")
+	# if tot2 > 1000 and set2 == 0:
+	# 	first2 = img2
+	# 	set2 = 1
+	# 	print("Changed Image 2")
 
 
+	cv2.namedWindow('Result1', cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('Result1', 800,600)
+	cv2.imshow("Result1", canvas1)
 
-	cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
-	cv2.resizeWindow('Result', 1800,1350)
-	cv2.imshow("Result", canvas)
+	cv2.namedWindow('Result2', cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('Result2', 800,600)
+	cv2.imshow("Result2", canvas2)
 
 	# cv2.namedWindow('First', cv2.WINDOW_NORMAL)
 	# cv2.resizeWindow('First', 1800,1350)
@@ -53,7 +82,7 @@ while True:
 	prevTime = curTime
 	total_time += diffTime
 	if total_time > 1:
-		print('FPS:',num_frames, total_time)
+		# print('FPS:',num_frames, total_time)
 		total_time = 0
 		num_frames = 0
 
